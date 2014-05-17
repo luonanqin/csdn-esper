810e1a0e76639cbafd7f8030b52854e7b22822cf
L.Big猩 <qinnanluo@sina.com>
2014-05-17 15:51:41 +0800

    update SplitDuplicateTest

--- csdn-esper/src/main/java/example/SplitDuplicateTest.java
+++ csdn-esper/src/main/java/example/SplitDuplicateTest.java
@@ -1,13 +1,13 @@
 package example;
 
-import java.io.Serializable;
-
 import com.espertech.esper.client.EPAdministrator;
 import com.espertech.esper.client.EPOnDemandQueryResult;
 import com.espertech.esper.client.EPRuntime;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
 import com.espertech.esper.client.EventBean;
+
+import java.io.Serializable;
 
 /**
  * Created by Luonanqin on 4/26/14.
@@ -39,7 +39,7 @@ class SplitEvent implements Serializable {
 	}
 
 	public String toString() {
-		return "SplitEvent{" + "size=" + size + ", quantity=" + price + '}';
+		return "SplitEvent{" + "size=" + size + ", price=" + price + '}';
 	}
 }
 
@@ -60,11 +60,28 @@ public class SplitDuplicateTest {
 		String epl6 = "create window SmallSize.win:keepall() (size int)";
 
 		// output first
-		String epl7 = "on " + splitEvent + " insert into LargeOrder select price where price > 8"
-				+ "insert into MediumOrder select price where price between 3 and 10 " + "insert into SmallOrder select price where price <= 3";
+		String insert1 = "insert into LargeOrder select price where price > 8";
+		String insert2 = "insert into MediumOrder select price where price between 3 and 10";
+		String insert3 = "insert into SmallOrder select price where price <= 3";
+		String epl7 = "on " + splitEvent + " " + insert1 + " " + insert2 + " " + insert3;
 
-		String epl8 = "on " + splitEvent + " insert into LargeSize select size where size > 5 "
-				+ "insert into MediumSize select size where size between 2 and 8 " + "insert into SmallSize select size where size <= 2 output all";
+		// output all
+		String insert4 = "insert into LargeSize select size where size > 5";
+		String insert5 = "insert into MediumSize select size where size between 2 and 8";
+		String insert6 = "insert into SmallSize select size where size <= 2";
+		String epl8 = "on " + splitEvent + " " + insert4 + " " + insert5 + " " + insert6 + " output all";
+
+		System.out.println("Output first(default): ");
+		System.out.println(insert1);
+		System.out.println(insert2);
+		System.out.println(insert3);
+
+		System.out.println();
+
+		System.out.println("Output all: ");
+		System.out.println(insert4);
+		System.out.println(insert5);
+		System.out.println(insert6);
 
 		String selectLargeOrder = "select * from LargeOrder";
 		String selectMediumOrder = "select * from MediumOrder";
@@ -73,6 +90,8 @@ public class SplitDuplicateTest {
 		String selectLargeSize = "select * from LargeSize";
 		String selectMediumSize = "select * from MediumSize";
 		String selectSmallSize = "select * from SmallSize";
+
+		System.out.println();
 
 		admin.createEPL(epl1);
 		admin.createEPL(epl2);
@@ -90,11 +109,16 @@ public class SplitDuplicateTest {
 		SplitEvent se4 = new SplitEvent(5, 6);
 		SplitEvent se5 = new SplitEvent(7, 9);
 
+		System.out.println(se4);
 		runtime.sendEvent(se1);
+		System.out.println(se1);
 		runtime.sendEvent(se2);
+		System.out.println(se2);
 		runtime.sendEvent(se3);
+		System.out.println(se3);
 		runtime.sendEvent(se4);
 		runtime.sendEvent(se5);
+		System.out.println(se5);
 
 		EPOnDemandQueryResult selectLOrder = runtime.executeQuery(selectLargeOrder);
 		EPOnDemandQueryResult selectMOrder = runtime.executeQuery(selectMediumOrder);
